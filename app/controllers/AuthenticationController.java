4985190351b5c46b9b58daae46fb658f0980f558
Sagar Gopale <sagar090989@yahoo.com>
2014-05-16 23:44:21 +0530

    Latest

--- app/controllers/AuthenticationController.java
+++ app/controllers/AuthenticationController.java
@@ -20,24 +20,11 @@ public class AuthenticationController extends Controller {
 
     @BodyParser.Of(BodyParser.Json.class)
     public static Result login() {
-        /*Login l = Ebean.find(Login.class).orderBy("created desc").setMaxRows(1).findUnique();
-
-            if(l == null) {
-                String _url = controllers.routes.Assets.at("images/mri.jpeg").absoluteURL(request());
-                return ok(views.html.index.render("Welcome", _url));
-            } else {
-                String _url = l.getImageUrl();
-                return ok(views.html.index.render("Welcome", _url));
-            }
-        } else {
-            return redirect(controllers.review.routes.ReviewController.getPatientsToReview());
-        }*/
         if (StringUtils.isEmpty(session("user"))) {
             return ok(views.html.index.render("Welcome"));
         } else {
             return redirect(controllers.data.routes.DataController.patientList());
         }
-
     }
 
     @BodyParser.Of(BodyParser.Json.class)
