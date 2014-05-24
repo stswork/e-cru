65e433e795dbefccece415f46c68b5bde0af3192
Sagar Gopale <sagar090989@yahoo.com>
2014-05-24 15:50:49 +0530

    Loader on form submit added

--- public/javascripts/form/events.js
+++ public/javascripts/form/events.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
     $("form").live("submit", function(e) {
-        $("#loader-a").attr("style","display:inline;");
         e.preventDefault();
         var _data = $(e.currentTarget).serialize();
         var _uri = $(e.currentTarget).data().uri;
+        $('.spinner').show();
         $.ajax({
             type: e.currentTarget.method,
             url: e.currentTarget.action,
             data: _data,
             success: function(data) {
                 $(e.currentTarget).find("div.message p").html(data.message);
-                $("#loader-a").attr("style","display:none;");
                 $(e.currentTarget).find("div.message").show();
                 setTimeout(function () {
                     $(e.currentTarget).find("div.message").hide();
+                    $('.spinner').hide();
                     window.location = _uri;
                 }, 3000);
                 $(e.currentTarget).get(0).reset();
@@ -21,9 +21,9 @@ $(document).ready(function () {
             error: function(xhr, textStatus, errorThrown) {
                 var em = jQuery.parseJSON(xhr.responseText)
                 $(e.currentTarget).find("div.message p").html(em.message);
-                $("#loader-a").attr("style","display:none;");
                 $(e.currentTarget).find("div.message").show();
                 setTimeout(function () {
+                    $('.spinner').hide();
                     $(e.currentTarget).find("div.message").hide();
                 }, 5000);
                 $(e.currentTarget).get(0).reset();
